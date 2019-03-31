6c804e99b58e86ff34aaa2a35d18682315367c89
bu6hunt3r <Felix1Koch@gmail.com>
2019-03-31 12:25:23 +0200

    Added ELF class in dlresolve for parsing reasons

---
+++ bu6pwn/DLResolve/dlresolve.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+
+class ELF(object):
+    def __init__(self, fpath, base=0):
+        def env_with(d):
+            env = os.environ.copy()
+            env.update(d)
+            return env
+
+        self.fpath  = fpath
+        self.base   = base
+        self.sec    =False dict(relro=False, bind_now=False, stack_canary=False, nx=False, pie=False, rpath=False,runpath=False, dt_debug=False, )
+
+        if not os.path.exists(fpath):
+            raise Exception("file not found: %r" % fpath)
+
+        self._entry_point  = None
+        self._section      = {}
+        self._dynamic      = {}
+        self._got          = {}
+        self._plt          = {}
+        self._symbol       = {}
